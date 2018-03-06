81bfaae8abc4d334b1823101cbebffb7ee761f9c
Johannes Christ <Johannes.christ@gmail.com>
2018-03-06 12:15:47 +0100

    Show online members on index page.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
+import requests
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    widget_data = requests.get("https://canary.discordapp.com/api/guilds/181866934353133570/widget.json").json()
+    online_members = len(widget_data['members'])
+    context = {
+        'online_members': online_members
+    }
+    return render(request, 'home/index.html', context)
