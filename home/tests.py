569e8646ee8bddbddf667c6f6b8c1234162028da
Johannes Christ <Johannes.christ@gmail.com>
2018-03-27 20:13:27 +0200

    Remove unneeded test case.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- home/tests.py
+++ home/tests.py
@@ -21,35 +21,6 @@ class AnonymousUserHomeAppTests(TestCase):
         self.assertFalse(user.is_superuser)
 
 
-class AnonymousUserWithMemberInDatabaseHomeAppTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        set_up_oauth_test_data(cls, [{
-            'id': '181866934353133570',
-            'permissions': 0x63584c0
-        }])
-
-    def test_index_status_200(self):
-        self.assertEqual(self.client.get(reverse('home:index')).status_code, 200)
-
-    def test_created_user_attributes_properly_set(self):
-        self.assertTrue(self.user.is_anonymous)
-        self.assertFalse(self.user.is_authenticated)
-        self.assertFalse(self.user.is_member)
-        self.assertFalse(self.user.is_staff)
-        self.assertFalse(self.user.is_superuser)
-
-    def test_context_user_attributes_properly_set(self):
-        res = self.client.get(reverse('home:index'))
-        user = res.context['user']
-
-        self.assertTrue(user.is_anonymous)
-        self.assertFalse(user.is_authenticated)
-        self.assertFalse(user.is_member)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-
 class AuthenticatedUserHomeAppTests(TestCase):
     @classmethod
     def setUpTestData(cls):
