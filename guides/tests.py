0599d09db62ffd6be8a698103a6f44fa7018695f
Johannes Christ <Johannes.christ@gmail.com>
2018-03-31 18:13:20 +0200

    Add tests for new guide views.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- guides/tests.py
+++ guides/tests.py
@@ -8,16 +8,21 @@ from .models import Guide
 from .views import IndexView
 
 
-GUIDE_CONTEXT_NAME = IndexView.context_object_name
+INDEX_GUIDE_CONTEXT_NAME = IndexView.context_object_name
 
 
 class GuidesModelNoGuidesTests(TestCase):
     def test_index_status_200(self):
-        self.assertEqual(self.client.get(reverse('guides:index')).status_code, 200)
+        resp = self.client.get(reverse('guides:index'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_detail_status_404(self):
+        resp = self.client.get(reverse('guides:detail', kwargs={'pk': 1}))
+        self.assertEqual(resp.status_code, 404)
 
     def test_no_guides_passes_empty_context_data(self):
-        res = self.client.get(reverse('guides:index'))
-        context_guides = res.context[GUIDE_CONTEXT_NAME]
+        resp = self.client.get(reverse('guides:index'))
+        context_guides = resp.context[INDEX_GUIDE_CONTEXT_NAME]
 
         self.assertFalse(context_guides.exists())
         self.assertSequenceEqual(context_guides, [])
@@ -35,7 +40,7 @@ class GuidesModelExistingGuidesTests(TestCase):
             title="Test Guide", overview="test",
             content="test", author=cls.author,
             pub_datetime=now
-        ),
+        )
         cls.guide_2 = Guide.objects.create(
             title="Test Guide 2", overview="test",
             content="test", author=cls.author,
@@ -45,8 +50,28 @@ class GuidesModelExistingGuidesTests(TestCase):
     def test_index_status_200(self):
         self.assertEqual(self.client.get(reverse('guides:index')).status_code, 200)
 
-    def test_guide_context_data_is_sorted(self):
-        res = self.client.get(reverse('guides:index'))
-        context_guides = res.context[GUIDE_CONTEXT_NAME]
+    def test_detail_status_200(self):
+        for guide in [self.guide_1, self.guide_2]:
+            resp = self.client.get(reverse('guides:detail', kwargs={'pk': guide.id}))
+            self.assertEqual(resp.status_code, 200)
+
+    def test_index_context_contains_created_guides(self):
+        resp = self.client.get(reverse('guides:index'))
+        self.assertIn(self.guide_1, resp.context[INDEX_GUIDE_CONTEXT_NAME])
+        self.assertIn(self.guide_2, resp.context[INDEX_GUIDE_CONTEXT_NAME])
+
+    def test_index_context_guides_sorted(self):
+        resp = self.client.get(reverse('guides:index'))
+        context_guides = resp.context[INDEX_GUIDE_CONTEXT_NAME]
 
         self.assertGreater(context_guides[0].pub_datetime, context_guides[1].pub_datetime)
+
+
+class AnonymousUserGuidesTests(TestCase):
+    def test_create_get_status_302(self):
+        resp = self.client.get(reverse('guides:create'))
+        self.assertEqual(resp.status_code, 302)
+
+    def test_create_post_status_302(self):
+        resp = self.client.post(reverse('guides:create'))
+        self.assertEqual(resp.status_code, 302)
