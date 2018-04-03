c5742ccdfc744c0588d850ae739f646a7dc3702e
Johannes Christ <Johannes.christ@gmail.com>
2018-04-03 23:57:31 +0200

    Move templates to follow Django's naming scheme.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- guides/views.py
+++ guides/views.py
@@ -1,20 +1,23 @@
-import os
-
 import requests
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .mixins import AuthorRequiredMixin, MemberRequiredMixin
+from .mixins import (
+    AddIsMemberContextMixin,
+    AddIsAdminContextMixin,
+    AuthorRequiredMixin,
+    MemberRequiredMixin
+)
 from .models import Guide
 from stats.models import Users as DiscordUser
 
 
-class IndexView(generic.ListView):
+class IndexView(AddIsMemberContextMixin, AddIsAdminContextMixin, generic.ListView):
     context_object_name = 'latest_guides'
     paginate_by = 10
-    template_name = 'guides/index.html'
 
     def get_queryset(self):
         return Guide.objects.order_by('-pub_datetime')
@@ -22,10 +25,14 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Guide
-    template_name = 'guides/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author_discord'] = DiscordUser.from_django_user(self.request.user)
+        return context
 
 
-class CreateView(generic.CreateView, MemberRequiredMixin):
+class CreateView(MemberRequiredMixin, generic.CreateView):
     model = Guide
     fields = ['title', 'overview', 'content']
 
@@ -35,11 +42,9 @@ class CreateView(generic.CreateView, MemberRequiredMixin):
         guide.save()
 
         detail_url = self.request.build_absolute_uri(reverse('guides:detail', kwargs={'pk': guide.id}))
-        webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+        webhook_url = settings.DISCORD_WEBHOOK_URL
         if webhook_url is not None:
             requests.post(webhook_url, json={
-                'username': 'Community Website',
-                'avatar_url': 'https://cdn.discordapp.com/emojis/410506329359253514.png?v=1',
                 'embeds': [{
                     'title': f'New Guide posted: "{guide.title}"',
                     'author': {
@@ -54,12 +59,11 @@ class CreateView(generic.CreateView, MemberRequiredMixin):
         return HttpResponseRedirect(detail_url)
 
 
-class EditView(generic.UpdateView, MemberRequiredMixin, AuthorRequiredMixin):
+class EditView(MemberRequiredMixin, AuthorRequiredMixin, generic.UpdateView):
     model = Guide
     fields = ['title', 'overview', 'content']
 
 
-class DeleteView(generic.DeleteView, MemberRequiredMixin, AuthorRequiredMixin):
+class DeleteView(MemberRequiredMixin, AuthorRequiredMixin, generic.UpdateView):
     model = Guide
     success_url = reverse_lazy('guides:index')
-
