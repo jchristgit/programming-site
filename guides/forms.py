6a717ba10a682d92df1016bbfb2dd6bd4c7f242c
Johannes Christ <Johannes.christ@gmail.com>
2018-04-03 14:19:45 +0200

    Remove now unneeded `forms` module.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- guides/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import Guide
-
-
-class GuideForm(ModelForm):
-    class Meta:
-        model = Guide
-        fields = ['title', 'overview', 'content']
