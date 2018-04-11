e6ddf35eb0f1963121c012f932685fb8bae175fd
Johannes Christ <Johannes.christ@gmail.com>
2018-04-11 23:16:09 +0200

    Remove dumb leftover from testing.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import ProfileView, NickRedirectView
+from .views import ProfileView
 
 
 app_name = 'profiles'
 urlpatterns = [
-    path('~<str:nick>', NickRedirectView.as_view(), name='nick_redirect'),
     path('<int:pk>', ProfileView.as_view(), name='detail')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,5 @@ from django.contrib.auth.models import User
 from django.views import generic
 
 
-
 class ProfileView(generic.DetailView):
     model = User
