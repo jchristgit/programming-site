768b47c49c9804fa2f121f47e910f231c506b81c
Johannes Christ <Johannes.christ@gmail.com>
2018-05-16 19:58:01 +0200

    Remove fixtures from `profiles` app.

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
-from django.test import TestCase, TransactionTestCase, override_settings
+from django.test import TestCase
 
 
 class AnonymousUserProfilesTests(TestCase):
@@ -15,65 +15,18 @@ class AnonymousUserProfilesTests(TestCase):
         self.assertEqual(resp.status_code, 404)
 
 
-class GuestUserProfilesTests(TransactionTestCase):
+class GuestUserProfilesTests(TestCase):
     """
     Scenario:
         - Guest User
         - accesses profile detail
     """
 
-    fixtures = ["guest_user"]
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user('testuser', password='testpassword')
 
     def setUp(self):
-        self.user = User.objects.first()
-        self.client.force_login(self.user)
-
-    def test_detail_status_200(self):
-        resp = self.client.get(reverse("profiles:detail", kwargs={"pk": self.user.id}))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_detail_context(self):
-        resp = self.client.get(reverse("profiles:detail", kwargs={"pk": self.user.id}))
-        self.assertEqual(resp.context["user"], self.user)
-
-
-@override_settings(DISCORD_GUILD_ID=42)
-class MemberUserProfilesTests(TransactionTestCase):
-    """
-    Scenario:
-        - Member User
-        - accesses profile detail
-    """
-
-    fixtures = ["member_user"]
-    multi_db = True
-
-    def setUp(self):
-        self.user = User.objects.first()
-        self.client.force_login(self.user)
-
-    def test_detail_status_200(self):
-        resp = self.client.get(reverse("profiles:detail", kwargs={"pk": self.user.id}))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_detail_context(self):
-        resp = self.client.get(reverse("profiles:detail", kwargs={"pk": self.user.id}))
-        self.assertEqual(resp.context["user"], self.user)
-
-
-@override_settings(DISCORD_GUILD_ID=42, DISCORD_ADMIN_ROLE_ID=10)
-class AdminUserProfilesTests(TransactionTestCase):
-    """
-    Scenario:
-        - Admin User
-        - accesses profile detail
-    """
-
-    fixtures = ["admin_user"]
-    multi_db = True
-
-    def setUp(self):
-        self.user = User.objects.first()
         self.client.force_login(self.user)
 
     def test_detail_status_200(self):
