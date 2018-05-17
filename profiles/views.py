ee86599ff9c463afe4007b478708836f6285127c
Johannes Christ <Johannes.christ@gmail.com>
2018-05-17 17:44:59 +0200

    Add an account deletion view.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,13 @@
 from django.conf import settings
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.views import generic
 
 from stats.models import RoleMembership
 
 
-class ProfileView(generic.DetailView):
+class ProfileDetailView(generic.DetailView):
     model = User
 
     def get_context_data(self, **kwargs):
@@ -18,3 +20,14 @@ class ProfileView(generic.DetailView):
             role__name="@everyone"
         )
         return context
+
+
+class ProfileDeleteView(UserPassesTestMixin, generic.DeleteView):
+    model = User
+    success_url = reverse_lazy('home:index')
+
+    raise_exception = True
+    permission_denied_message = "You are not allowed to delete a profile that isn't yours."
+
+    def test_func(self):
+        return self.request.user == self.get_object()
