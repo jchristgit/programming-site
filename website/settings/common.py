2ace3263f884f758b52d6f43bac2db6832627d20
Johannes Christ <jc@jchri.st>
2018-06-16 00:55:43 +0200

    Remove `XFrameOptionsMiddleware` (handled by nginx).

--- website/settings/common.py
+++ website/settings/common.py
@@ -57,8 +57,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware"
 ]
 
 ROOT_URLCONF = "website.urls"
