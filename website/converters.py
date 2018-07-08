b5fb46dfe007650a3dc1ae5f1c2675b1adfaa506
Johannes Christ <jc@jchri.st>
2018-07-08 11:35:52 +0200

    Allow `id`s in headers.

--- website/converters.py
+++ website/converters.py
@@ -3,10 +3,11 @@ import markdown
 from bleach_whitelist import markdown_attrs, markdown_tags
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.toc import TocExtension
 
 
 # https://python-markdown.github.io/extensions/
-MARKDOWN_EXTENSIONS = (CodeHiliteExtension(), FencedCodeExtension())
+MARKDOWN_EXTENSIONS = (CodeHiliteExtension(), FencedCodeExtension(), TocExtension())
 
 
 def markdownify(html: str) -> str:
@@ -14,6 +15,7 @@ def markdownify(html: str) -> str:
     Given a string of HTML, returns the string converted
     to markdown and run through a HTML sanitizer.
     Syntax highlighting and fenced code blocks are supported.
+    Additionally, a table of contents can be inserted by using `[TOC]`.
     """
 
     return bleach.clean(
